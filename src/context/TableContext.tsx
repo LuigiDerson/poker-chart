7c1f4b266e3c8478fc3305c8325ef4ac8c1361ec
Luigi Sanchez <luigi.prodesign@gmail.com>
2021-06-03 19:55:42 +0100

    get highest card

--- src/context/TableContext.tsx
+++ src/context/TableContext.tsx
@@ -36,12 +36,21 @@ interface TableContextProps {
   children: ReactNode
 }
 
-const createTableState = (defaultTable: TableState) => {
-  const pictureCards = { A: 1, T: 10, J: 11, Q: 12, K: 13 }
+type PictureCardsType = {
+  [key: string]: number
+}
 
+const getHighestCard = (a: string, b: string) => {
+  const pictureCards: PictureCardsType = { A: 1, T: 10, J: 11, Q: 12, K: 13 }
+  const x = pictureCards[a] || +a
+  const y = pictureCards[b] || +b
+  return x > y ? `${a}${b}` : `${b}${a}`
+}
+
+const createTableState = (defaultTable: TableState) => {
   for (let r = 0; r < cards.length; r++) {
     for (let c = 0; c < cards.length; c++) {
-      const pair = cards[r] + cards[c]
+      const pair = getHighestCard(cards[r], cards[c])
       const cell = { pair: pair, actions: [] }
       defaultTable.byId[pair] = cell
       defaultTable.allIds.push(pair)
